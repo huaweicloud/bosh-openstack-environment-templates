d3352b7aaea1aa027c3c538b125f5354f9b7b715
zhongjun2 <jun.zhongjun2@gmail.com>
2018-08-16 20:34:13 +0800

    Update resources.tf

--- bosh-init-tf/resources.tf
+++ bosh-init-tf/resources.tf
@@ -36,6 +36,17 @@ resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_6" {
   security_group_id = "${openstack_networking_secgroup_v2.secgroup.id}"
 }
 
+resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_7" {
+  region = "${var.region_name}"
+  direction = "ingress"
+  ethertype = "IPv4"
+  protocol = "tcp"
+  port_range_min = 0
+  port_range_max = 0
+  remote_ip_prefix = "0.0.0.0/0"
+  security_group_id = "${openstack_networking_secgroup_v2.secgroup.id}"
+}
+
 resource "openstack_networking_secgroup_rule_v2" "secgroup_rule_5" {
   region = "${var.region_name}"
   direction = "ingress"
